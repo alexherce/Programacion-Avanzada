5a8fadddef19e9c0290b0284f339fd42bb231ea1
Alejandro Herce <alejandro@herce.me>
2016-02-11 13:16:23 -0600

    archivos creados

---
+++ tarea2/ejercicio5/ejercicio5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ejercicio5
+//
+//  Created by Alejandro Herce on 2/11/16.
+//  Copyright © 2016 Alejandro Herce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
