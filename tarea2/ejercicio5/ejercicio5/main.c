cf196f71fbed9893b88b97c7d73cdf367977ee14
Alejandro Herce <alejandro@herce.me>
2016-02-11 14:07:30 -0600

    t2e5 listo

--- tarea2/ejercicio5/ejercicio5/main.c
+++ tarea2/ejercicio5/ejercicio5/main.c
@@ -7,9 +7,47 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int n = 1;
+    int c;
+    int i;
+    int estado;
+    char* programa = NULL;
+    pid_t pid;
+    
+    while ((c = getopt(argc, argv, "n:p:")) != -1) {
+        switch (c) {
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 'p':
+                programa = optarg;
+                break;
+            default:
+                return 1;
+        }
+    }
+    
+    for(i = 0; i < n; i++)
+    {
+        pid = fork();
+        
+        if (pid == -1)
+        {
+            printf("Error al crear el hijo. \n");
+        }
+        else if (pid == 0)
+        {
+            execl(programa, (char*) NULL);
+            exit(0);
+        }
+    }
+    wait(NULL);
+    
     return 0;
 }
