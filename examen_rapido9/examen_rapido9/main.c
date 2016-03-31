5f06cb2b12860ff03db407e2c5dafe3e1a21bc00
Alejandro Herce <alejandro@herce.me>
2016-03-31 10:55:01 -0600

    examen 2-4

---
+++ examen_rapido9/examen_rapido9/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  examen_rapido9
+//
+//  Created by Alejandro Herce on 3/31/16.
+//  Copyright © 2016 Alejandro Herce. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
